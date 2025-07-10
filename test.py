18204cee98545ddf6cf04f44ff243c128a62c197
ArjunSNair00 <snairarjun43@gmail.com>
2025-07-11 01:32:20 +0530

    Last Sync: 2025-07-11 01:32 (Mobile)

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+'''
+test project for data science involving numpy, pandas, matplotlib
+
+'''
+
 import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
-print(arr)
+sum=0
+print("Array:",arr)
+for i in arr:
+	sum+=i
+print("Sum:",sum)
