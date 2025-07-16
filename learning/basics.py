377c3a52e2faf4c24e8ce461b5b776851b5fc61a
Arjun S Nair <snairarjun43@gmail.com>
2025-07-16 21:44:55 +0530

    pg1

--- learning/basics.py
+++ learning/basics.py
@@ -2,4 +2,7 @@ a=3 #constant stored in a variable, variable is a container
 print(a) #display value in a
 print('a') #displaying the letter 'a'
 
-#BASICS: 100
+# BASICS: 
+# print 1 to 100, in intervals of 2
+# eg: 1,3,5....
+# step size: 2
