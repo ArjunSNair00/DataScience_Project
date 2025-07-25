094629450811cc5ee072bcbd23572349fdb305aa
ArjunSNair00 <snairarjun43@gmail.com>
2025-07-25 08:41:44 +0530

    Last Sync: 2025-07-25 08:41 (Mobile)

---
+++ Arjun/Marks/simplest_sine_wave_possible_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 1, 0.001)
+print("x values\n")
+print(x)
+y = np.sin(2*np.pi*x)
+print("\ny values\n")
+print(y)
+
+plt.plot(x, y)
+plt.title("Sine Wave")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
